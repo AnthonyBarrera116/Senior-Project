d516e3c1ac2658ca948d6e275e201c32f981e217
Anthony Barrera <55001338+AnthonyBarrera116@users.noreply.github.com>
2023-02-28 18:06:38 -0500

    Add files via upload

--- Server.js
+++ Server.js
@@ -1,7 +1,11 @@
-const expApp = require('./App.js');
-let hostname = 'localhost';
-let port = 4000;
+//const dotenv = require('dotenv');
+//dotenv.config();
+require('dotenv').config();
+const dbcon = require('./dbConnect');
+dbcon.connect();
 
-const server= expApp.app.listen(port,hostname,function(){
-   console.log(`Server running in ${hostname}:${port}`);
+const ExpressApp = require('./app.js');
+
+ExpressApp.app.listen(process.env.PORT,process.env.HOSTNAME,function(){ // Listen to client requests in hostname:port
+    console.log(`Server Running on ${process.env.HOSTNAME}:${process.env.PORT}...`);
 });
