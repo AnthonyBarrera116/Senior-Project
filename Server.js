8af640471d7d0fce2547f626faa463f89f50eb5a
nrvogel543 <nrvogel@loyola.edu>
2023-03-03 15:44:06 -0500

    Fix Files

--- Server.js
+++ Server.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const dbcon = require('./dbConnect');
 dbcon.connect();
 
-const ExpressApp = require('./app.js');
+const ExpressApp = require('./App.js');
 
 ExpressApp.app.listen(process.env.PORT,process.env.HOSTNAME,function(){ // Listen to client requests in hostname:port
     console.log(`Server Running on ${process.env.HOSTNAME}:${process.env.PORT}...`);
