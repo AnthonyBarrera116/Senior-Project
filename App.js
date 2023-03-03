4dfb90e65631541e88ba1349dddc665f484fb804
nvogel543 <59944827+nvogel543@users.noreply.github.com>
2023-03-03 15:45:42 -0500

    Delete App.js

--- App.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const morgan = require('morgan');
-exports.app = express();
-exports.app.use(morgan('dev'));
-
-exports.app.use(express.static('View'));
-
-exports.app.use(express.urlencoded({extended: true}));
-exports.app.use(express.json());
