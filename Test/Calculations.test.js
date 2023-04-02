0e916e0e6f0fe7a1c4f825013e18ea7c99cb4b70
nrvogel543 <nrvogel@loyola.edu>
2023-04-02 16:21:05 -0400

    Calculate Future Value #3

--- Test/Calculations.test.js
+++ Test/Calculations.test.js
@@ -5,3 +5,8 @@ test('Calculate PV', async function()
     expect(calculations.PV(0.08, 6, 0, 12200, 0)).toBe("-7688.07");
     expect(calculations.PV(0, 6, 0, 12200, 0)).toBe(-12200);
 });
+
+test('Calculate FV', async function()
+{
+    expect(calculations.FV(0.06, 25, 0, 4800, 0)).toBe(-20600.98);
+});
--- View/Calculations.js
+++ View/Calculations.js
@@ -1,5 +1,5 @@
 //Based off of the function from this repo: https://github.com/handsontable/formula.js
-PV = function(rate, nper, pmt, fv, type) {
+exports.PV = function(rate, nper, pmt, fv, type){
     fv = fv || 0;
   
     rate = parseFloat(rate);
@@ -12,4 +12,27 @@ PV = function(rate, nper, pmt, fv, type) {
     } else {
       return ((((1 - Math.pow(1 + rate, nper)) / rate) * pmt * (1 + rate * type) - fv) / Math.pow(1 + rate, nper)).toFixed(2);
     }
+  };
+
+//Based off of the function from this repo: https://github.com/handsontable/formula.js
+  exports.FV = function(rate, nper, pmt, pv, type){
+    pv = pv || 0;
+
+    rate = parseFloat(rate);
+    nper = parseFloat(nper);
+    pmt = parseFloat(pmt);
+    pv = parseFloat(pv);
+  
+    let result;
+    if (rate === 0) {
+      result = pv + pmt * nper;
+    } else {
+      let term = Math.pow(1 + rate, nper);
+      if (type === 1) {
+        result = pv * term + pmt * (1 + rate) * (term - 1) / rate;
+      } else {
+        result = pv * term + pmt * (term - 1) / rate;
+      }
+    }
+    return -(result).toFixed(2);
   };
