5443b16d748c53d26005b7e5984eeb0bd7a8cb4f
nrvogel543 <nrvogel@loyola.edu>
2023-04-02 17:31:11 -0400

    Fix Calculations.js

--- View/Calculations.js
+++ View/Calculations.js
@@ -1,5 +1,5 @@
 //Based off of the function from this repo: https://github.com/handsontable/formula.js
-PV = function(rate, nper, pmt, fv, type) {
+exports.PV = function(rate, nper, pmt, fv, type) {
   fv = fv || 0;
 
   rate = parseFloat(rate);
@@ -14,7 +14,30 @@ PV = function(rate, nper, pmt, fv, type) {
   }
 };
 
-bp = function(ym, mr, bfv, acr, type) {
+//Based off of the function from this repo: https://github.com/handsontable/formula.js
+exports.FV = function(rate, nper, pmt, pv, type){
+  pv = pv || 0;
+
+  rate = parseFloat(rate);
+  nper = parseFloat(nper);
+  pmt = parseFloat(pmt);
+  pv = parseFloat(pv);
+
+  let result;
+  if (rate === 0) {
+    result = pv + pmt * nper;
+  } else {
+    let term = Math.pow(1 + rate, nper);
+    if (type === 1) {
+      result = pv * term + pmt * (1 + rate) * (term - 1) / rate;
+    } else {
+      result = pv * term + pmt * (term - 1) / rate;
+    }
+  }
+  return -(result).toFixed(2);
+};
+
+exports.bp = function(ym, mr, bfv, acr, type) {
 
   ym = parseFloat(ym);
   mr = parseFloat(mr);
