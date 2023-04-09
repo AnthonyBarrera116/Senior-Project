59b0bd21d27023630ce5ab0b778a486ae69b1d7c
nrvogel543 <nrvogel@loyola.edu>
2023-04-09 10:48:02 -0400

    Add tests

--- Test/Calculations.test.js
+++ Test/Calculations.test.js
@@ -16,3 +16,17 @@ test('Calculate BP', async function()
     expect(calculations.BP(9, 8, 1000, .06, 1)).toBe("873.41");
 });
 
+test('Calculate NPV', async function()
+{
+    expect(calculations.NPV([0.028, 500, 600, 800])).toBe("1790.54");
+});
+
+test('Calculate PMT', async function()
+{
+    expect(calculations.PMT(0.07, 3, 0, 7500)).toBe(-2332.89);
+});
+
+test('Calculate NPER', async function()
+{
+    expect(calculations.NPER(0.05, 2000, 0, 7500, 0)).toBe("-4.26");
+});
